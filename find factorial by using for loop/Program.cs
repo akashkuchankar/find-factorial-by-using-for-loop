9e5c09677f6459cd50ff851eaf380d1e2f315f5f
akashkuchankar <kuchankarakash@gmail.com>
2022-10-05 10:50:11 -0700

    Add project files.

---
+++ find factorial by using for loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_factorial_by_using_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            int Fact = 1;
+            for (int i = 1; i <= N; i++) 
+            {
+                Fact = Fact * i;
+                
+            }
+            Console.WriteLine(Fact);
+
+
+        }
+    }
+}
